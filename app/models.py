774866f97c5c5315265b1cdff36924d3790b0be5
IsaacBargh <bargh.isaac@gmail.com>
2024-06-13 11:25:29 +1200

    created routes for navbar and linked pages to database

--- app/models.py
+++ app/models.py
@@ -1,12 +1,34 @@
 from app.routes import db
 
 
+class Month(db.Model):
+    __tablename__ = "Month"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text())
+
+    def __repr__(self):
+        return self.name
+
+
 class Constellation(db.Model):
     __tablename__ = "Constellation"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text())
     description = db.Column(db.Text())
     story = db.Column(db.Text())
+    image = db.Column(db.Text())
+
+    def __repr__(self):
+        return self.name
+
+
+class Planet(db.Model):
+    __tablename__ = "Planet"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text())
+    description = db.Column(db.Text())
+    constellation = db.Column(db.Integer, db.ForeignKey("Constellation.id"))
+    constellation_desc = db.relationship("Constellation", backref="Planet")
     image = db.Column(db.Text())
 
     def __repr__(self):
--- app/routes.py
+++ app/routes.py
@@ -4,18 +4,37 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir,"night.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, "night.db")
 db = SQLAlchemy()
 db.init_app(app)
 
 import app.models as models
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
+@app.route('/all_months')
+def all_months():
+    months = models.Month.query.all()
+    return render_template('all_months.html', months=months)
+
+
+@app.route('/all_planets')
+def all_planets():
+    planets = models.Planet.query.all()
+    return render_template('all_planets.html', planets=planets)
+
+
 @app.route('/all_stars')
 def all_stars():
     stars = models.Star.query.all()
     return render_template('all_stars.html', stars=stars)
+
+
+@app.route('/all_constellations')
+def all_constellations():
+    constellations = models.Constellation.query.all()
+    return render_template('all_constellations.html', constellations=constellations)
